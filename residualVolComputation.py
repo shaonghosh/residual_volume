b4b34ebf2a9fa0e3a88e9224925ec3e0d27ec377
Shaon Ghosh <shaon.ghosh@ligo.org>
2018-08-12 21:19:57 -0500

    Added computation of volume difference between central ball and box

--- residualVolComputation.py
+++ residualVolComputation.py
@@ -6,7 +6,7 @@ from scipy.special import gamma
 from scipy.optimize import fsolve
 
 
-def residualVol(N):
+def residualVol(N, residual=True):
     '''
     This function computes the residual volume left in the unit side n-box after putting
     n-balls in each of the corners of the n-box and a n-ball that just touches the
@@ -18,6 +18,9 @@ def residualVol(N):
     ballVol = Total volume of portions of the n-balls enclosed in the box (effective volume)
     centralBallVol = Volume of the largest n-ball that can fit at the center of box
 	resVol = Volume left
+
+    Returns :: The residual volume or the difference between volume of the box and the
+               central ball
     '''
     boxVol = 2**N
     # Vol of n-Ball from  https://en.wikipedia.org/wiki/Volume_of_an_n-ball #
@@ -25,17 +28,30 @@ def residualVol(N):
     radius = np.sqrt(N) - 1 # Radius of the central n-ball
     centralBallVol = (np.pi**(N/2))/(gamma(N/2 + 1))*(radius**N)
     resVol = boxVol - ballVol - centralBallVol
-    return resVol
+    resVol_central = boxVol - centralBallVol
+    if residual:
+        return resVol
+    return resVol_central
 
 
 if __name__ == "__main__":
     dims = np.arange(2, 9)
     resVols = residualVol(dims)
-    pl.plot(dims, resVols, 'r-o')
+    resVols_central = residualVol(dims, residual=False)
+    p1, = pl.plot(dims, resVols, 'r-o')
+    p2, = pl.plot(dims, resVols_central, 'b-o')
+    pl.legend([p1, p2], ['Residual volume', 'Volume of box - central ball'])
+
     pl.xlabel('Number of dimensions')
     pl.ylabel('Volume left after putting all the balls')
     pl.grid(1)
     zero_of_residualVol = fsolve(residualVol, 6, xtol=1e-12)
+    zero_of_residualVol_cental = fsolve(residualVol, 6, args=(False), xtol=1e-12)
+
     print('The residual volume vanishes at D = {}'.format(zero_of_residualVol[0]))
+    print('The volume of the central ball exceeds volume of box at D = {}'.format(zero_of_residualVol_cental[0]))
+
     pl.show()
+
+
 
